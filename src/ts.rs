13adcdd5aeb3758b8210d4d975d48ec8f4559403
Pawel Bogut <pbogut@pbogut.me>
2023-09-18 15:54:19 +0200

    refactor: split code into modules

---
+++ src/ts.rs
@@ -0,0 +1,15 @@
+use lsp_types::{Position, Range};
+use tree_sitter::Node;
+
+pub fn get_range_from_node(node: Node) -> Range {
+    Range {
+        start: Position {
+            line: node.start_position().row as u32,
+            character: node.start_position().column as u32,
+        },
+        end: Position {
+            line: node.end_position().row as u32,
+            character: node.end_position().column as u32,
+        },
+    }
+}
