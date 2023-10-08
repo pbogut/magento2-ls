e3b19f025debfb51376b45d4b986acb9fc3dfb9f
Pawel Bogut <pbogut@pbogut.me>
2023-10-08 14:47:07 +0200

    refactor: split definition module

---
+++ src/lsp/definition/component.rs
@@ -0,0 +1,58 @@
+use std::path::{Path, PathBuf};
+
+use lsp_types::Location;
+
+use crate::{
+    m2::{M2Item, M2Path},
+    state::ArcState,
+};
+
+use super::path_to_location;
+
+pub fn find_plain(state: &ArcState, comp: &str) -> Vec<Location> {
+    let mut result = vec![];
+    let workspace_paths = state.lock().workspace_paths();
+    for path in workspace_paths {
+        let path = path.append(&["lib", "web", comp]).append_ext("js");
+        if let Some(location) = path_to_location(&path) {
+            result.push(location);
+        }
+    }
+    result
+}
+pub fn find_rel(comp: String, path: &Path) -> Option<Vec<Location>> {
+    let mut path = path.join(comp);
+    path.set_extension("js");
+    path_to_location(&path).map(|location| vec![location])
+}
+
+pub fn mod_location(
+    mod_name: String,
+    file_path: &str,
+    mod_path: PathBuf,
+    path: &PathBuf,
+) -> Vec<Location> {
+    let mut result = vec![];
+    let components = vec![M2Item::ModComponent(
+        mod_name.clone(),
+        file_path.to_string(),
+        mod_path,
+    )];
+
+    let area = path.get_area();
+
+    for component in components {
+        if let M2Item::ModComponent(_, file_path, mod_path) = component {
+            for area_path in area.path_candidates() {
+                let comp_path = mod_path
+                    .append(&["view", area_path, "web", &file_path])
+                    .append_ext("js");
+                if let Some(location) = path_to_location(&comp_path) {
+                    result.push(location);
+                }
+            }
+        }
+    }
+
+    result
+}
---
+++ src/lsp/definition/phtml.rs
@@ -0,0 +1,78 @@
+use lsp_types::Location;
+
+use crate::{
+    m2::{M2Area, M2Path},
+    state::ArcState,
+};
+
+use super::path_to_location;
+
+pub fn find_admin(state: &ArcState, mod_name: &str, template: &str) -> Vec<Location> {
+    let mut result = vec![];
+    add_phtml_in_mod_location(state, &mut result, mod_name, template, &M2Area::Adminhtml);
+    add_phtml_in_admin_theme_location(state, &mut result, mod_name, template);
+    result
+}
+
+pub fn find_front(state: &ArcState, mod_name: &str, template: &str) -> Vec<Location> {
+    let mut result = vec![];
+    add_phtml_in_mod_location(state, &mut result, mod_name, template, &M2Area::Frontend);
+    add_phtml_in_front_theme_location(state, &mut result, mod_name, template);
+    result
+}
+
+pub fn find_base(state: &ArcState, mod_name: &str, template: &str) -> Vec<Location> {
+    let mut result = vec![];
+    add_phtml_in_mod_location(state, &mut result, mod_name, template, &M2Area::Base);
+    add_phtml_in_front_theme_location(state, &mut result, mod_name, template);
+    add_phtml_in_admin_theme_location(state, &mut result, mod_name, template);
+    result
+}
+
+fn add_phtml_in_mod_location(
+    state: &ArcState,
+    result: &mut Vec<Location>,
+    mod_name: &str,
+    template: &str,
+    area: &M2Area,
+) {
+    let mod_path = state.lock().get_module_path(mod_name);
+    if let Some(path) = mod_path {
+        for area in area.path_candidates() {
+            let templ_path = path.append(&["view", area, "templates", template]);
+            if let Some(location) = path_to_location(&templ_path) {
+                result.push(location);
+            }
+        }
+    }
+}
+
+fn add_phtml_in_admin_theme_location(
+    state: &ArcState,
+    result: &mut Vec<Location>,
+    mod_name: &str,
+    template: &str,
+) {
+    #[allow(clippy::significant_drop_in_scrutinee)]
+    for theme_path in state.lock().list_admin_themes_paths() {
+        let path = theme_path.append(&[mod_name, "templates", template]);
+        if let Some(location) = path_to_location(&path) {
+            result.push(location);
+        }
+    }
+}
+
+fn add_phtml_in_front_theme_location(
+    state: &ArcState,
+    result: &mut Vec<Location>,
+    mod_name: &str,
+    template: &str,
+) {
+    #[allow(clippy::significant_drop_in_scrutinee)]
+    for theme_path in state.lock().list_front_themes_paths() {
+        let path = theme_path.append(&[mod_name, "templates", template]);
+        if let Some(location) = path_to_location(&path) {
+            result.push(location);
+        }
+    }
+}
